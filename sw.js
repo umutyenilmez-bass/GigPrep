86d18d5518bd05003e5e24512a22f22ebf89dc5b
Umut Yenilmez <umutyenilmez@gmail.com>
2026-04-28 05:55:47 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'gigprep-v1';
+const ASSETS = ['./index.html', './logo.png', './manifest.json', './friz-quadrata-regular.ttf', './friz-quadrata-bold-italic.ttf'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html'))));
+});
